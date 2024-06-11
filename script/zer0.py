650ab3bd2a038f04a76daf4c8ceb2e64fbb7987e
bamr87 <10567847+bamr87@users.noreply.github.com>
2024-06-10 22:17:48 -0700

    zer0 recursion

--- script/zer0.py
+++ script/zer0.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-def convert_md_to_bash(md_file_path, bash_file_path):
-    with open(md_file_path, 'r') as md_file, open(bash_file_path, 'w') as bash_file:
-        bash_file.write('#!/bin/bash\n')  # add shebang line
-        shell_script_mode = False
-        for line in md_file:
-            if '```shell' in line:
-                shell_script_mode = True
-                continue
-            elif '```' in line and shell_script_mode:
-                shell_script_mode = False
-                continue
-            if shell_script_mode:
-                bash_file.write(line)
-            else:
-                bash_file.write(f'# {line}')
+import os
 
-convert_md_to_bash('zer0.md', 'zer0.sh')
+def convert_md_to_files(md_file_path):
+    language_files = {}
+    language_mode = None
+    language_extensions = {'python': '.py', 'shell': '.sh'}
+    shebang_lines = {'python': '#!/usr/bin/env python3\n', 'shell': '#!/bin/bash\n'}
+
+    output_folder = "script"
+    os.makedirs(output_folder, exist_ok=True)
+
+    with open(md_file_path, 'r') as md_file:
+        for line in md_file:
+            if line.startswith('```'):
+                if language_mode:
+                    language_mode = None
+                else:
+                    language = line.strip('`\n')
+                    if language in language_extensions:
+                        language_mode = language
+                        if language not in language_files:
+                            output_file_path = os.path.join(output_folder, os.path.basename(md_file_path).replace('.md', language_extensions[language]))
+                            language_file = open(output_file_path, 'w')
+                            if language in shebang_lines:
+                                language_file.write(shebang_lines[language])
+                            language_files[language] = language_file
+                continue
+
+            if language_mode:
+                language_files[language_mode].write(line)
+
+    for language_file in language_files.values():
+        language_file.close()
+
+convert_md_to_files('zer0.md')
--- script/zer0.sh
+++ script/zer0.sh
@@ -5,9 +5,10 @@ system_profiler SPHardwareDataType | awk '/Model Name:|Model Identifier:|Model N
 system_profiler SPSoftwareDataType | awk '/System Version:|Kernel Version:/ {print $0}'
 
 # install and update prerequisites
+
 brew install git
 brew install gh
-brew install docker
+brew install --cask docker
 brew install --cask visual-studio-code
 # Or use the following to set the environment variables
 
@@ -15,94 +16,87 @@ export GITHOME=~/github
 export GHUSER=bamr87
 export GIT_REPO=zer0-mistakes
 export ZREPO=$GITHOME/$GIT_REPO
+# Confirm the environment variables by echoing them and logging them to a file
 
-# Confirm the environment variables by echoing them
-
-echo $GITHOME # /Users/bamr87/github
-echo $GHUSER # bamr87
-echo $GIT_REPO # zer0-mistakes
-echo $ZREPO # /Users/bamr87/github/zer0-mistakes
+echo "$(date) - Log started" > env-variables.log
+echo -e "GITHOME: $GITHOME\nGHUSER: $GHUSER\nGIT_REPO: $GIT_REPO\nZREPO: $ZREPO" >> env-variables.log
 # Set your Git email and name to tag your commits
+# If the GIT_ID is set, use it to tag your commits. "https://github.com/settings/emails"
 
-git config --global user.email "$GHUSER@users.noreply.github.com"
 git config --global user.name "$GHUSER"
-# If you didnt already set it in the previous step
-# FIXME: quotes in comments dont work
-
-echo "What is your Github ID?"
-read GIT_ID
-
-# Set your email using ID
-
-git config --global user.email "$GIT_ID+$GHUSER@users.noreply.github.com"
+git config --global user.email "$GHUSER@users.noreply.github.com"
+if [ -n "$GIT_ID" ]; then
+  git config --global user.email "$GIT_ID+$GHUSER@users.noreply.github.com"
+fi
 # confirm your email
 
-git config -l
-
-# Initialize your github repository
-
-gh repo create $GHUSER/$GIT_REPO --public
+git config -l | tee -a env-variables.log
 # Create your github home directory and repo
 
 mkdir -p $ZREPO
+# Initialize your github repository
+
+gh repo create $GIT_REPO --gitignore Jekyll -l mit --public
 # If new repo, initialize it
 
 cd $ZREPO
 git init
-echo "# Building new report from $ZREPO" >> README.md
-git add README.md
-git commit -m "first commit"
-git branch -M main
 git remote add origin https://github.com/${GHUSER}/${GIT_REPO}.git
-git push -u origin main
+git pull origin main
 
+# Create a README.md file based on the zer0.md file from this repo
+curl https://raw.githubusercontent.com/bamr87/it-journey/master/zer0.md > README.md
+git add README.md
+git commit -m "Init $GIT_REPO"
+git branch -M main
+git push -u origin main
 # Open your new github repository in the browser
 
 open https://github.com/${GHUSER}/${GIT_REPO}
 
+# Create a new Gemfile
 cd $ZREPO
-jekyll new ./ --force
-bundle install
-# If running MacOS
-bundle add webrick
-bundle install
-jekyll serve
-cd $ZREPO
-bundle update
-bundle install
-jekyll serve
-code _config.yml
-cd $ZREPO
-wget https://raw.githubusercontent.com/bamr87/it-journey/master/favicon.ico
-bundle lock --add-platform x86-mingw32 x64-mingw32 x86-mswin32 java
-# find theme path
-bundle info --path minima
-JEKYLL_THEME=$(bundle info --path minima)
-echo $JEKYLL_THEME
-cd $JEKYLL_THEME
+touch Gemfile
 
-cp -aR $JEKYLL_THEME/* $ZREPO
-bundle remove jekyll-theme-minima
-#_config.yml
-# Build settings
-# theme: minima
-plugins:
-  - jekyll-feed
-bundle remove minima --install
-jekyll serve
-{%- raw -%}
-cd $ZREPO
-mkdir _layout
-cd _layout
-echo "{{ content }}" >> default.html 
-{% endraw %}
-#tree #alias #zshrc #profile
-alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
-echo alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'" >> ~/.zshrc
+# Write the non-commented lines to the Gemfile
+echo 'source "https://rubygems.org"' >> Gemfile
+echo "gem 'github-pages' , '231'" >> Gemfile
+echo "gem 'jekyll' , '3.9.5'" >> Gemfile
+echo "gem 'jekyll-theme-zer0' , '0.1.2'" >> Gemfile
+echo "group :jekyll_plugins do" >> Gemfile
+echo "  gem 'jekyll-feed', \"~> 0.17\"" >> Gemfile
+echo "  gem 'jekyll-sitemap' , \"~> 1.4.0\"" >> Gemfile
+echo "  gem 'jekyll-seo-tag', \"~> 2.8.0\"" >> Gemfile
+echo "  gem 'jekyll-paginate', \"'~> 1.1'\"" >> Gemfile
+echo "end" >> Gemfile
+# Download the _config.yml file from the jekyll-theme-zer0 repo
 
-tree
-cd -
-d=$(date +%Y-%m-%d)
-echo "$d"
-cd $ZREPO/_posts
-wget -O $d-home.md https://raw.githubusercontent.com/bamr87/it-journey/master/home.md 
+curl https://raw.githubusercontent.com/bamr87/zer0-mistakes/main/_config.yml > _config.yml
+# Create a new Dockerfile
+cd $ZREPO
+touch Dockerfile
+
+# Write the content to the Dockerfile
+echo "# Use an official Ruby runtime as a parent image" >> Dockerfile
+echo "FROM ruby:2.7.4" >> Dockerfile
+echo "# escape=\\" >> Dockerfile
+echo "ENV GITHUB_GEM_VERSION 231" >> Dockerfile
+echo "ENV JSON_GEM_VERSION 1.8.6" >> Dockerfile
+echo "ENV GIT_REPO ${GIT_REPO}" >> Dockerfile
+echo "WORKDIR /app" >> Dockerfile
+echo "ADD . /app" >> Dockerfile
+echo "RUN gem update --system 3.3.22" >> Dockerfile
+echo "RUN bundle update" >> Dockerfile
+echo "RUN bundle install" >> Dockerfile
+echo "RUN bundle clean --force" >> Dockerfile
+echo "EXPOSE 4000" >> Dockerfile
+echo 'CMD ["bundle", "exec", "jekyll", "serve", "--host", "0.0.0.0"]' >> Dockerfile
+# build the docker image based on the Dockerfile
+docker build -t ${GIT_REPO} .
+# Run the container in detached mode
+docker run -d -p 4000:4000 -v ${ZREPO}:/app --name zer0_container ${GIT_REPO}
+
+# Start the container and run the CMD line from the Dockerfile
+docker start zer0_container
+
+open http://localhost:4000/
